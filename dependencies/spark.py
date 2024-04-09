91aaece7d745825a2e3c2c38bf999403620fdd10
Erik Tastepe <erik.tastepe@gmail.com>
2024-04-09 14:11:15 -0400

    initial commit

---
+++ dependencies/spark.py
@@ -0,0 +1,56 @@
+"""
+spark.py
+~~~~~~~~
+
+Module containing helper function for use with Apache Spark
+"""
+
+from os import listdir, path
+import json
+from pyspark import SparkFiles
+from pyspark.sql import SparkSession
+
+
+def start_spark(
+    app_name="pyspark-demo",
+):
+    """Start Spark session, get Spark logger and load config files.
+
+    Start a Spark session on the worker node and register the Spark
+    application with the cluster. Note, that only the app_name argument
+    will apply when this is called from a script sent to spark-submit.
+    All other arguments exist solely for testing the script from within
+    an interactive Python console.
+
+    :param app_name: Name of Spark app.
+    :return: A tuple of references to the Spark session, logger and
+        config dict (only if available).
+    """
+
+    spark_builder = SparkSession.builder.appName(app_name)
+
+    # create session and retrieve Spark logger object
+    spark = spark_builder.getOrCreate()
+
+    sc = spark.sparkContext
+    log4jLogger = sc._jvm.org.apache.log4j
+    spark_logger = log4jLogger.LogManager.getLogger("pyspark-logger")
+
+    # get config file if sent to cluster with --files
+    spark_files_dir = SparkFiles.getRootDirectory()
+    config_files = [
+        filename
+        for filename in listdir(spark_files_dir)
+        if filename.endswith("config.json")
+    ]
+
+    if config_files:
+        path_to_config_file = path.join(spark_files_dir, config_files[0])
+        with open(path_to_config_file, "r") as config_file:
+            config_dict = json.load(config_file)
+        spark_logger.warn("loaded config from " + config_files[0])
+    else:
+        spark_logger.warn("no config file found")
+        config_dict = None
+
+    return spark, spark_logger, config_dict
---
+++ src/etl.py
@@ -0,0 +1,81 @@
+"""
+etl_job.py
+~~~~~~~~~~
+
+Example Apache Spark ETL job definition.
+"""
+
+import pandas as pd
+import argparse
+from pyspark.sql.functions import sum
+
+from dependencies.spark import start_spark
+
+
+def main(data_source: str, output_uri: str):
+    """Main ETL script definition.
+
+    :return: None
+    """
+    # start Spark application and get Spark session, logger and config
+    spark, log, _config = start_spark(app_name="pyspark-demo")
+
+    # log that main ETL job is starting
+    log.warn("etl_job is up-and-running")
+
+    # Extract from S3
+    data = extract_data(spark, data_source)
+
+    # Transform
+    data = transform_data(data)
+
+    # Load into S3
+    load_data(data, output_uri)
+
+    # log the success and terminate Spark application
+    log.warn("etl_job is finished")
+    spark.stop()
+    return None
+
+
+def extract_data(spark, data_source: str):
+    """Extract data from CSV.
+
+    :param spark: Spark session object.
+    :return: Spark DataFrame.
+    """
+    df = spark.read.option("header", "true").csv(data_source)
+
+    return df
+
+
+def transform_data(df: pd.DataFrame):
+    """Transform original dataset.
+
+    :param df: Input DataFrame.
+    :return: Transformed DataFrame.
+    """
+    df = df.groupby("NEIGHBORHOOD").agg(sum("TOTAL UNITS").alias("TOTAL UNITS SUM"))
+
+    return df
+
+
+def load_data(df: pd.DataFrame, output_uri: str):
+    """Write transformed dataframe to S3 bucket output URI.
+
+    :param df: DataFrame to save.
+    :param output_uri: Output URI for the dataframe
+    :return: None
+    """
+    df.write.mode("overwrite").parquet(output_uri)
+    return None
+
+
+# entry point for PySpark ETL application
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_source")
+    parser.add_argument("--output_uri")
+    args = parser.parse_args()
+
+    main(args.data_source, args.output_uri)
